809dae98a820152b618c3eb0147130c18f70601f
Artem Serediuk <a.serediuk@student.csn.khai.edu>
2022-09-24 12:25:12 +0300

    Dropped skill description column

---
+++ EducationPortal.DataAccess/Migrations/20220924092336_DeleteSkillDescription.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EducationPortal.DataAccess.Migrations
+{
+    public partial class DeleteSkillDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Skills");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Skills",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
